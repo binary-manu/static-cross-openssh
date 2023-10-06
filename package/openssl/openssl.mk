04d3dcee423a07d5fc58dbb22085c2750414e4d0
binary-manu <47382117+binary-manu@users.noreply.github.com>
2023-10-06 15:29:16 +0200

    Skip OpenSSL alpha versions

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,8 +4,8 @@ define openssl/determine_latest
     . ./version.sh;
 	list_github_tags https://github.com/openssl/openssl |
 	sed -En
-	  -e '/^OpenSSL_1/{s/^OpenSSL_(.*)$$/\1/; y/_/./; p}; # Matches OpenSSL 1.x versions'
-	  -e '/^openssl-3/{s/^openssl-(.*)$$/\1/;         p}; # Matches OpenSSL 3.x versions' |
+	  -e '/^OpenSSL_1/{s/^OpenSSL_(.*)$$/\1/; y/_/./;   p}; # Matches OpenSSL 1.x versions'
+	  -e '/^openssl-3/{s/^openssl-(.*)$$/\1/; /alpha/d; p}; # Matches OpenSSL 3.x versions' |
 	sort_versions | tail -n 1
   ))
 endef
