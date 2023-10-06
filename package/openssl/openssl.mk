84a083b94e9a8f28c9c794bf1dd48af3817e9f41
binary-manu <47382117+binary-manu@users.noreply.github.com>
2023-10-06 15:22:09 +0200

    Also use OpenSSL 3.x if available

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -3,7 +3,9 @@ define openssl/determine_latest
   $(eval override openssl/VERSION := $(shell
     . ./version.sh;
 	list_github_tags https://github.com/openssl/openssl |
-	sed -En '/^OpenSSL_1/{s/^OpenSSL_(.*)$$/\1/; y/_/./; p}' |
+	sed -En
+	  -e '/^OpenSSL_1/{s/^OpenSSL_(.*)$$/\1/; y/_/./; p}; # Matches OpenSSL 1.x versions'
+	  -e '/^openssl-3/{s/^openssl-(.*)$$/\1/;         p}; # Matches OpenSSL 3.x versions' |
 	sort_versions | tail -n 1
   ))
 endef
